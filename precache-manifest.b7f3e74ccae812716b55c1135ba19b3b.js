a673300723ea6593aa4787b963a9871b8d2162e7
Girish Godage <girishgodage@gmail.com>
2022-02-14 11:46:24 +0530

    Updates

---
+++ precache-manifest.b7f3e74ccae812716b55c1135ba19b3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8aaf7ccb438859c712d99f73320bc1b",
+    "url": "/adviceapp/index.html"
+  },
+  {
+    "revision": "b990c31bc84b4fa91620",
+    "url": "/adviceapp/static/css/main.ac28993e.chunk.css"
+  },
+  {
+    "revision": "be8764e90a80b8a08290",
+    "url": "/adviceapp/static/js/2.bf44d81a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/adviceapp/static/js/2.bf44d81a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b990c31bc84b4fa91620",
+    "url": "/adviceapp/static/js/main.4b0b677c.chunk.js"
+  },
+  {
+    "revision": "9ce2a1006d61d22d30f6",
+    "url": "/adviceapp/static/js/runtime-main.3ac178d1.js"
+  },
+  {
+    "revision": "40e0b04848ef145f8772bf2aa3161ce9",
+    "url": "/adviceapp/static/media/mumbaicity.40e0b048.jpg"
+  }
+]);
